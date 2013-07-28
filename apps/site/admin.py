e5bdd1c34c4a3959fa8a2cf7e7d022952c4addf8
aassaaa <v.zabiiaka@gmail.com>
2013-07-28 22:47:43 +0300

    Slug is readonly if it is created

--- apps/site/admin.py
+++ apps/site/admin.py
@@ -4,8 +4,18 @@ from . import models as m
 
 
 class PageAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("title",)}
     radio_fields = {"category": admin.VERTICAL}
+    prepopulated_fields = {"slug": ("title",)}
+
+    def get_prepopulated_fields(self, request, obj=None):
+        if obj is not None:
+            return {}
+        return self.prepopulated_fields
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ['slug']
+        return []
 
 
 class ClientAdmin(admin.ModelAdmin):
