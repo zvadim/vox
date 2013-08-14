b7ebf43e571c360107466064660b8699f4475243
aassaaa <v.zabiiaka@gmail.com>
2013-08-15 00:39:56 +0300

    nl_semicolon filter

--- apps/site/templatetags/site_filters.py
+++ apps/site/templatetags/site_filters.py
@@ -8,6 +8,6 @@ def roman_month(month):
     m = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII']
     return m[int(month)-1]
 
-@register.filter(name='nl_comma')
-def nl_comma(string):
-    return '<br>'.join(string.split(','))
+@register.filter(name='nl_semicolon')
+def nl_semicolon(string):
+    return '<br>'.join(string.split(';'))
